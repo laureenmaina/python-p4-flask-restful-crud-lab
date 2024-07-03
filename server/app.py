d7f62830bfe8f4605adef9f5d7807f9b13c06f58
Laurine Maina <mainalaureen58@gmail.com>
2024-07-03 19:10:25 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -46,7 +46,39 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        data = request.get_json()
+        
+        if plant:
+            for attr, value in data.items():
+                setattr(plant, attr, value)
 
+            db.session.add(plant)
+            db.session.commit()
+
+            response_dict = plant.to_dict()
+            response = make_response(response_dict, 200)
+            return response
+        else:
+            return make_response({"message": "Plant not found"}, 404)
+    
+    def delete(self,id):
+        plant=Plant.query.filter_by(id=id).first()
+
+        if plant:
+            db.session.delete(plant)
+            db.session.commit()
+
+            response=make_response('',204)
+            return response
+        
+        else:
+            response = make_response(jsonify({"message": "Plant not found"}), 404)
+            return response
+
+      
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
